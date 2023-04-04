ecdd67a13fb3c46cee5aecef9bb8702c44276ef8
Asheesh Choksi <asheesh.choksi@gmail.com>
2023-04-04 12:50:44 +0530

    Initial commit

---
+++ test/lambda-layer-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaLayerDemo from '../lib/lambda-layer-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-layer-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaLayerDemo.LambdaLayerDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
