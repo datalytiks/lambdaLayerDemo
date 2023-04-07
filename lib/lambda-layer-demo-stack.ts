40a42ed1fa8cdf1d95a12f7ca4a25cd7ed82282f
Asheesh Choksi <asheesh.choksi@gmail.com>
2023-04-07 09:41:04 +0530

    Initial

--- lib/lambda-layer-demo-stack.ts
+++ lib/lambda-layer-demo-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+
 
 export class LambdaLayerDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create Lambda Layer - package-layer
+    const packageLayer = new lambda.LayerVersion(this, 'package-layer', {
+      compatibleRuntimes: [ lambda.Runtime.NODEJS_16_X ],
+      compatibleArchitectures: [ lambda.Architecture.X86_64 ],
+      code: lambda.Code.fromAsset('src/layers/package-layer'),
+      description: 'npm package layer having yup'
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaLayerDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create Lambda Layer - package-layer
+    const functionLayer = new lambda.LayerVersion(this, 'function-layer', {
+      compatibleRuntimes: [ lambda.Runtime.NODEJS_16_X ],
+      compatibleArchitectures: [ lambda.Architecture.X86_64 ],
+      code: lambda.Code.fromAsset('src/layers/function-layer'),
+      description: 'function layer having calc module'
+    })
+
+    // Create Lambda - Lambda for demo layer
+    const lambdaForLayerDemo = new lambda.Function(this, 'lambdaForLayerDemo', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset('src/lambdas/lambda-one'),
+      handler: 'demoLayerLambda.main',
+      layers: [packageLayer, functionLayer]
+    })
+
+    // Create API gateway
+    new apigw.LambdaRestApi(this, 'LayerDemo', {
+      handler: lambdaForLayerDemo,
+    })
+
+
+
   }
 }
